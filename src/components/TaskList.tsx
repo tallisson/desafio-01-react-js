f4e66c758ae01850d352da0eb92e4fcad883e671
Thiago Allisson <allissonribeiro02@gmail.com>
2021-12-22 01:40:54 -0300

    Versão 01 do desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,30 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const task = {
+      id: tasks.length + 1,
+      title: newTaskTitle,
+      isComplete: false
+    };    
+    setTasks(oldState => [...oldState, task]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(oldState => 
+      oldState.map(task => {
+        if(task.id === id) {
+          task.isComplete = !task.isComplete;
+        }
+        return task;
+      })
+    );
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(oldState => oldState.filter(task => task.id !== id));
   }
 
   return (
